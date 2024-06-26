850c1184f35473c9bfc2f766c8c72e50d55fcc1b
Gabriel Kloppel <tkloppel@gmail.com>
2024-06-26 17:55:06 -0300

    Aula-04 Anatomia das Classes-4

---
+++ java-anatomia-classe/src/MinhaClasse4.java
@@ -0,0 +1,16 @@
+public class MinhaClasse4 {
+
+    public static void main(String[] args) {
+        //Com Identação
+
+        int mediaFinal = 10;
+		if (mediaFinal < 6)
+			System.out.println("REPROVADO");
+		else if (mediaFinal == 6)
+			System.out.println("PROVA MINERVA");
+		else
+			System.out.println("APROVADO");
+
+    }
+    
+    }
