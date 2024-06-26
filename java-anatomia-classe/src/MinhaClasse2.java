bc20cc542f091b336c5dc19999a718b3ee69744f
Gabriel Kloppel <tkloppel@gmail.com>
2024-06-26 16:55:19 -0300

    Aula-02 Anatomia das Classes-2

---
+++ java-anatomia-classe/src/MinhaClasse2.java
@@ -0,0 +1,31 @@
+public class MinhaClasse2 {
+
+ public static void main(String[] args) {
+    int ano = 2021;
+    ano = 2022;
+    final String BR = "Brasil";
+    double PI = 3.14;
+    int ESTADOS_BRASILEIRO = 27;
+    int ANO_2000 = 2000;
+    //Recomendações: Para declarar uma variável nós podemos utilizar caracteres, números e símbolos, porém devemos seguir algumas regras da linguagem.
+    //Deve conter apenas letras, _ (underline), $ ou os números de 0 a 9
+    //Deve obrigatoriamente se iniciar por uma letra (preferencialmente), _ ou $, jamais com número
+    //Deve iniciar com uma letra minúscula (boa prática – ver abaixo)
+    //Não pode conter espaços
+    //Não podemos usar palavras-chave da linguagem
+    //O nome deve ser único dentro de um escopo
+
+// Declação inválida de variáveis
+    //int numero&um = 1; //Os únicos símbolos permitidos são _ e $
+    //int 1numero = 1;    //Uma variável não pode começar com númerico
+    //int numero um = 1; //Não pode ter espaço no nome da variável
+    //int long = 1; //long faz parte das palavras reservadas da linguagem
+ // Declaração válida de veriáveis
+    int numero$um = 1;
+    int numero1 = 1;
+    int numeroum = 1;
+    int longo = 1;
+
+ }   
+    
+}
